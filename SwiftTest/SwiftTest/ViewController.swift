6f569357d14fe5829bea3e44f995cc46c620445d
Thor Stenbæk <thorstenbaek@gmail.com>
2017-06-26 17:39:34 +0200

    Setup project

---
+++ SwiftTest/SwiftTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftTest
+//
+//  Created by Thor Stenbæk on 26.06.2017.
+//  Copyright © 2017 Thor Stenbæk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
